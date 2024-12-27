e76ada34f0ed0c78de212fc7b8dc23f3121cbf5e
Danny Peelen <dapeelen@gmail.com>
2024-12-27 12:13:48 -0500

    working on image

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
 export default function App(){
+  const BASE_URL = import.meta.env.BASE_URL;
   return(
   <div class="row">
 
     <div class="leftside animate__animated animate__fadeInUp">
-      <img src="src/me.jpg" alt="Me!"></img>
+      <img src={BASE_URL+"src/me.jpg"} alt="Me!"></img>
       <h3>Danny Peelen</h3>
       <ul>
         <li><i class="bi bi-linkedin"> </i><a href="https://linkedin.com/in/daniel-peelen">LinkedIn</a></li>
